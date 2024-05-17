c1240034e5b7863caa9e876b4704f4ed7c007902
Donghyeon0915 <dongdong99@kakao.com>
2024-05-17 15:33:24 +0900

    feat(setting) : 임시 액티비티 추가
    * 빌드 확인용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ val properties = Properties().apply {
 
 plugins {
     wepli("android.application")
+    alias(libs.plugins.kotlin.android) // TODO feature 모듈 추가 후 제거 필요
 }
 
 android {
@@ -53,10 +54,11 @@ android {
 }
 
 dependencies {
-
+    // TODO feature 모듈 추가 후 제거 필요
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
