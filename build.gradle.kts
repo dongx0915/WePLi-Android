5c8927125c008db093fa6fd80ac5470598c04219
Donghyeon0915 <dongdong99@kakao.com>
2024-05-17 14:22:28 +0900

    feat(versionCatalog) : 플러그인 선언
    * Version Catalog에 선언된 plugin은 프로젝트 build.gradle.kts에서 alias로 정의 해주어야함
    * alias로 정의만 해놓고 적용은 x (필요한 모듈에서 적용)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,29 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        // 플러그인을 사용할 수 있도록 클래스 패스에 추가
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.android.gradle.plugin)
+        classpath(libs.hilt.gradle.plugin)
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    /**
+     * 플러그인 정의 (적용은 x)
+     * Version Catalog에 작성한 plugins 들을 모두 정의
+     * 필요할 때 특정 모듈에서만 적용할 수 있음
+     */
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.dagger.hilt) apply false
 }
