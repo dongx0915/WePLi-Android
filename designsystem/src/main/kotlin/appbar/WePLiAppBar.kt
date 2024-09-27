94c1969f5df1cdc8ef8942fb0eeccfb9159bd6cb
dongx._.2 <dongdong99@kakao.com>
2024-09-27 22:40:25 +0900

    feat(ui) : WePLiAppBar 추가

--- designsystem/src/main/kotlin/appbar/WePLiAppBar.kt
+++ designsystem/src/main/kotlin/appbar/WePLiAppBar.kt
@@ -1,64 +1,159 @@
 package appbar
 
-import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.wepli.wepli.designsystem.R
+import custom.NotificationDot
+import theme.WePLiTheme
 
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WePLiAppBar() {
-    TopAppBar(
-        title = { Text(text = "WePLi") },
-        colors = TopAppBarDefaults.topAppBarColors(
-            titleContentColor = Color.White,
-            containerColor = Color.Black
-        ),
+fun WePLiAppBar(
+    title: String = "",
+    containerColor: Color = Color.Black,
+    contentsColor: Color = Color.White,
+    showLogo: Boolean = false,
+    showBackButton: Boolean = false,
+    showSearchButton: Boolean = false,
+    showNotificationButton: Boolean = false,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    onClickBack: (() -> Unit)? = null,
+    onClickSearch: (() -> Unit)? = null,
+    onClickNotification: (() -> Unit)? = null,
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = WePLiTheme.typo.subTitle3,
+                color = Color.White
+            )
+        },
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "",
-                    tint = Color.White,
-                    modifier = Modifier.padding(start = 16.dp)
+            if (showLogo) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_wepli_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(44.dp)
+                        .padding(vertical = 14.dp, horizontal = 10.dp)
+                )
+            }
+
+            if (showBackButton) {
+                AppBarIcon(
+                    iconResource = R.drawable.ic_arrow_back,
+                    onClick = { onClickBack?.invoke() }
                 )
             }
         },
         actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "",
-                    tint = Color.White
+            if (showSearchButton) {
+                AppBarIcon(
+                    iconResource = R.drawable.ic_search,
+                    onClick = { onClickSearch?.invoke() }
                 )
             }
 
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "",
-                    tint = Color.White
+            if (showNotificationButton) {
+                AppBarIcon(
+                    iconResource = R.drawable.ic_alarm,
+                    onClick = { onClickNotification?.invoke() },
+                    badgeVisible = true
                 )
             }
-        }
+        },
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = containerColor,
+            navigationIconContentColor = contentsColor,
+            actionIconContentColor = contentsColor,
+            titleContentColor = contentsColor
+        )
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun WePLiAppBarPreview() {
-    WePLiAppBar()
+fun AppBarIcon(
+    modifier: Modifier = Modifier,
+    @DrawableRes iconResource: Int,
+    iconColor: Color = WePLiTheme.color.white,
+    badgeVisible: Boolean = false,
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = { onClick.invoke() },
+        modifier = modifier.size(40.dp)
+    ) {
+        Box {
+            Icon(
+                painter = painterResource(id = iconResource),
+                tint = iconColor,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(20.dp)
+                    .align(Alignment.Center)
+            )
+
+            if (badgeVisible) {
+                NotificationDot(
+                    modifier = Modifier
+                        .size(5.dp) // Notification Dot의 크기
+                        .align(Alignment.TopEnd) // 오른쪽 상단에 위치
+                        .offset(x = 3.dp, y = 0.dp) // 아이콘과 약간의 간격을 위한 오프셋 조정
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = false)
+@Composable
+fun AppBarPreview() {
+    Column(
+        modifier = Modifier.background(Color.Transparent)
+    ) {
+        WePLiAppBar(
+            showLogo = true,
+            showBackButton = false,
+            showSearchButton = true,
+            showNotificationButton = true,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = false)
+@Composable
+fun MainAppBarPreview() {
+    Column(
+        modifier = Modifier.background(Color.Transparent)
+    ) {
+        WePLiAppBar(
+            title = "타이틀",
+            showBackButton = true,
+        )
+    }
 }
