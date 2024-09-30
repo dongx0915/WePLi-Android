66c64874cc15ea813184c43f4c1ba20f07336175
dongx._.2 <dongdong99@kakao.com>
2024-09-30 16:49:51 +0900

    feat(ext) : dpToSp 확장 함수 추가

--- core/common/src/main/kotlin/extensions/ComposableExt.kt
+++ core/common/src/main/kotlin/extensions/ComposableExt.kt
@@ -3,8 +3,10 @@ package extensions
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 
 @Composable
-fun Dp.toPx(): Int = LocalDensity.current.run {
-    this@toPx.roundToPx()
-}
+fun Dp.toPx(): Int = with(LocalDensity.current) { this@toPx.roundToPx() }
+
+@Composable
+fun Dp.toSp(): TextUnit = with(LocalDensity.current) { this@toSp.toSp() }
