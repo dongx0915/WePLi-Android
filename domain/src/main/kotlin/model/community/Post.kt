5260af7d95f3316ced0c75169ab9d666610c17f6
dongx._.2 <dongdong99@kakao.com>
2024-10-08 00:21:08 +0900

    feat(community) : 게시글 클래스에 프로필 이미지 속성 추가

--- domain/src/main/kotlin/model/community/Post.kt
+++ domain/src/main/kotlin/model/community/Post.kt
@@ -7,11 +7,13 @@ import model.music.Song
  * @property title 제목
  * @property content 내용
  * @property author 작성자
+ * @property profileImg 작성자 프로필 이미지
  * @property songList 게시글에 포함된 음악 리스트
  */
 data class Post(
     val title: String,
     val content: String,
     val author: String,
+    val profileImg: String,
     val songList: List<Song>
 )
