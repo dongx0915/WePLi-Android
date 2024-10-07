83c1d644bf4e57ea86bfbb1ec948e0259d564a66
dongx._.2 <dongdong99@kakao.com>
2024-10-07 23:42:25 +0900

    feat(ext) : Activity 확장 함수 추가

---
+++ core/common/src/main/kotlin/common/ActivityExt.kt
@@ -0,0 +1,52 @@
+package common
+
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+
+/**
+ * 기존 액티비티를 fade out 과 함께 닫고, 새로운 액티비티를 fade in 과 함께 시작합니다.
+ *
+ * @param intentBuilder 새로 시작할 액티비티의 [Intent]
+ */
+inline fun <reified T : Activity> Activity.changeActivityWithAnimation(
+    intentBuilder: Intent.() -> Intent = { this },
+) {
+    startActivityWithAnimation<T>(
+        intentBuilder = intentBuilder,
+        withFinish = true,
+    )
+}
+
+/**
+ * Fade in 과 함께 새로운 액티비티를 시작합니다.
+ *
+ * @param intentBuilder 새로 시작할 액티비티의 [Intent]
+ * @param withFinish 현재 액티비티를 종료할지 여부
+ */
+inline fun <reified T : Activity> Activity.startActivityWithAnimation(
+    intentBuilder: Intent.() -> Intent = { this },
+    withFinish: Boolean = false,
+) {
+    startActivity(intentBuilder(Intent(this, T::class.java)))
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    if (withFinish) finish()
+}
+
+/**
+ * Fade in 과 함께 새로운 Launcher 를 시작합니다.
+ *
+ *  * @param intentBuilder 새로 시작할 액티비티의 [Intent]
+ *  * @param withFinish 현재 액티비티를 종료할지 여부
+ */
+inline fun <reified T : Activity> Activity.startLauncherWithAnimation(
+    launcher: ActivityResultLauncher<Intent>,
+    intentBuilder: Intent.() -> Intent = { this },
+    withFinish: Boolean = false,
+) {
+    launcher.launch(
+        intentBuilder(Intent(this, T::class.java)),
+    )
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    if (withFinish) finish()
+}
