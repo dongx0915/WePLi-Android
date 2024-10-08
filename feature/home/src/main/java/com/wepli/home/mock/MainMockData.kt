c413cc8cee094833c04c9cdab78ff1fd427200f7
dongx._.2 <dongdong99@kakao.com>
2024-10-09 01:28:08 +0900

    feat(mock) : Mock 데이터 수정

--- feature/home/src/main/java/com/wepli/home/mock/MainMockData.kt
+++ feature/home/src/main/java/com/wepli/home/mock/MainMockData.kt
@@ -27,7 +27,7 @@ val musicMockData = listOf(
 
 val recommendPlaylistMockData = listOf(
     RecommendPlaylist("To. X", "https://cdnimg.melon.co.kr/cm2/album/images/113/72/896/11372896_20231127104957_500.jpg"),
-    RecommendPlaylist("주저하는 연인들을 위해", "https://cdnimg.melon.co.kr/cm/album/images/102/60/858/10260858_500.jpg"),
+    RecommendPlaylist("주저하는 연인들을 위해", "https://image.bugsm.co.kr/album/images/500/202371/20237198.jpg?version=20210422183751.0"),
     RecommendPlaylist("내가 S면 넌 나의 N이 되어줘", "https://cdnimg.melon.co.kr/cm2/album/images/115/05/319/11505319_20240624143253_500.jpg"),
     RecommendPlaylist("ETA", "https://cdnimg.melon.co.kr/cm2/album/images/112/81/456/11281456_20230706180841_500.jpg"),
     RecommendPlaylist("너의 모든 순간", "https://cdnimg.melon.co.kr/cm/album/images/022/32/505/2232505_500.jpg"),
