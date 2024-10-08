2d003eff270bcdc7adb20e75f342f5520dd9ba22
dongx._.2 <dongdong99@kakao.com>
2024-10-09 01:29:13 +0900

    refactor(community) : 커뮤니티 패키지 정리

--- feature/community/src/main/kotlin/com/community/wepli/community/navigator/CommunityNavigatorImpl.kt
+++ feature/community/src/main/kotlin/com/community/wepli/community/navigator/CommunityNavigatorImpl.kt
@@ -2,7 +2,7 @@ package com.community.wepli.community.navigator
 
 import android.app.Activity
 import android.content.Intent
-import com.community.wepli.community.CommunityActivity
+import com.community.wepli.community.main.CommunityActivity
 import com.wepli.navigator.feature.community.CommunityNavigator
 import common.startActivityWithAnimation
 import javax.inject.Inject
