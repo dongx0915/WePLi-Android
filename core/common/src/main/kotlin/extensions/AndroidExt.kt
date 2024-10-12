f692175f4fdd9ecaaff97d8452d1298993523d7f
dongx._.2 <dongdong99@kakao.com>
2024-10-13 00:51:16 +0900

    feat(ext) : getParcelableExtraCompat 확장 함수 추가

--- core/common/src/main/kotlin/extensions/AndroidExt.kt
+++ core/common/src/main/kotlin/extensions/AndroidExt.kt
@@ -1,6 +1,9 @@
 package extensions
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.os.Parcelable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowInsetsControllerCompat
@@ -10,4 +13,12 @@ fun Activity.setStatusBarColor(color: Color, darkIcons: Boolean = true) {
     window.statusBarColor = color.toArgb()
     // 상태바 아이콘 색상을 설정 (어두운 아이콘/밝은 아이콘)
     WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = darkIcons
+}
+
+inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, T::class.java)
+    } else {
+        getParcelableExtra(key)
+    }
 }
