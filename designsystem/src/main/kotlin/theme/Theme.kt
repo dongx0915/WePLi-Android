dfb67af2825e192fd9251d602103c1045f450d48
dongx._.2 <dongdong99@kakao.com>
2024-11-07 18:01:53 +0900

    feat(ui) : 시스템 하단 바 검은색으로 설정

--- designsystem/src/main/kotlin/theme/Theme.kt
+++ designsystem/src/main/kotlin/theme/Theme.kt
@@ -1,14 +1,21 @@
 package theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.Density
+import androidx.core.view.WindowInsetsControllerCompat
 
 @Composable
 fun WePLiTheme(
@@ -17,6 +24,20 @@ fun WePLiTheme(
 ) {
     val colorScheme = remember(darkTheme) {
         if (darkTheme) darkColors() else lightColors()
+    }
+
+    // 시스템 하단바 색상 설정
+    val view = LocalView.current
+    val window = (view.context as? Activity)?.window
+    if (window != null) {
+        LaunchedEffect(darkTheme) {
+            // 하단 네비게이션 바의 배경색을 검은색으로 설정
+            window.navigationBarColor = Color.Black.toArgb()
+
+            // 다크 모드에 따른 아이콘 색상 설정
+            val controller = WindowInsetsControllerCompat(window, view)
+            controller.isAppearanceLightNavigationBars = !darkTheme
+        }
     }
 
     CompositionLocalProvider(
