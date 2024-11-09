137ab3e0003bd5dbb843277cad3f3b5a7dfd37d3
dongx._.2 <dongdong99@kakao.com>
2024-11-09 13:07:46 +0900

    feat(nav) : CommunityGraph, Controller 정의

---
+++ feature/community/src/main/kotlin/com/wepli/community/navigation/CommunityNavigation.kt
@@ -0,0 +1,52 @@
+package com.wepli.community.navigation
+
+import android.net.Uri
+import android.util.Log
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.wepli.community.detail.CommunityDetailScreen
+import com.wepli.community.detail.CommunityDetailViewModel
+import com.wepli.community.main.CommunityScreen
+import com.wepli.navigator.feature.community.CommunityRoute
+import com.wepli.shared.feature.uimodel.community.PostUiData
+import extensions.parseFromJson
+import extensions.toJsonString
+
+// Controller - 화면 이동을 담당
+fun NavController.navigateToCommunityDetail(post: PostUiData) {
+    navigate("${CommunityRoute.Detail.route}/${Uri.encode(post.toJsonString())}")
+}
+
+// Graph - 도착 지점(화면)을 정의
+fun NavGraphBuilder.communityMainGraph(
+    navOnCommunityDetail: (PostUiData) -> Unit
+) {
+    composable(CommunityRoute.Home.route) {
+        CommunityScreen(
+            navOnCommunityDetail = { post -> navOnCommunityDetail(post) }
+        )
+    }
+}
+
+fun NavGraphBuilder.communityDetailGraph() {
+    composable(
+        route = "${CommunityRoute.Detail.route}/{post}",
+        arguments = listOf(
+            navArgument("post") { type = NavType.StringType }
+        )
+    ) {
+        val post = it.arguments?.getString("post")?.parseFromJson<PostUiData>()
+        val viewModel = hiltViewModel<CommunityDetailViewModel>()
+
+        LaunchedEffect(Unit) {
+            viewModel.setPost(post ?: PostUiData())
+        }
+
+        CommunityDetailScreen(viewModel)
+    }
+}
