b0f7e00c99f53251a8be61e5e558a8308982d8e7
dongx._.2 <dongdong99@kakao.com>
2024-11-12 15:57:51 +0900

    fix(nav) : 화면 전환 전에 하단바 사라지는 현상 수정

--- app/src/main/java/com/wepli/MainActivity.kt
+++ app/src/main/java/com/wepli/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,9 +22,12 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -71,8 +76,22 @@ fun MainApp() {
         derivedStateOf { state.value?.destination?.route }
     }
 
-    val isBottomTabVisible by remember {
+    val isBottomTabVisibleTarget by remember {
         derivedStateOf { currentRoute in bottomNavItems.map { it.route } }
+    }
+
+    var isBottomTabVisible by remember { mutableStateOf(true) }
+    val visibilityAnimationProgress by animateFloatAsState(
+        targetValue = if (isBottomTabVisibleTarget) 1f else 0f,
+        label = ""
+    )
+
+    // 애니메이션이 끝난 후 isBottomTabVisible 상태 변경
+    LaunchedEffect(visibilityAnimationProgress) {
+        when(visibilityAnimationProgress) {
+            0f -> isBottomTabVisible = false
+            1f -> isBottomTabVisible = true
+        }
     }
 
     Scaffold(
