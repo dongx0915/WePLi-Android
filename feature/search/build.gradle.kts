9d0310c42cf064655bc590a4bd0d093fbb739d5b
dongx._.2 <dongdong99@kakao.com>
2025-01-01 18:43:38 +0900

    feat(module) : Search 모듈 추가

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    wepli("android.feature")
+    wepli("android.compose")
+    wepli("android.hilt")
+}
+
+android {
+    namespace = "com.wepli.feature.search"
+}
---
+++ feature/search/src/main/java/com/wepli/search/screen/SearchMusicScreen.kt
@@ -0,0 +1,137 @@
+package com.wepli.search
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import appbar.WepliAppBar
+import theme.WepliTheme
+
+@Composable
+fun SearchScreenRoute() {
+    SearchScreen()
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreen() {
+    Scaffold(
+        containerColor = WepliTheme.color.black,
+        topBar = {
+            WepliAppBar(
+                showLogo = false,
+                showBackButton = true,
+                title = "곡 검색"
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+            .padding(paddingValues)
+            .padding(horizontal = 20.dp)
+        ) {
+            Box(modifier = Modifier.padding(vertical = 10.dp)) {
+                WepliTextField(
+                    query = "",
+                    onQueryUpdate = { },
+                    placeholderText = "검색어를 입력하세요.",
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(),
+                    contentPadding = PaddingValues(all = 16.dp),
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WepliTextField(
+    modifier: Modifier,
+    query: String,
+    onQueryUpdate: (String) -> Unit,
+    placeholderText: String = "",
+    keyboardOptions: KeyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    contentPadding: PaddingValues = PaddingValues(all = 16.dp),
+) {
+    BasicTextField(
+        modifier = modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(WepliTheme.color.gray000),
+        value = query,
+        onValueChange = { onQueryUpdate(it) },
+        textStyle = WepliTheme.typo.body2.copy(
+            color = WepliTheme.color.gray900,
+        ),
+        decorationBox = @Composable { innerTextField ->
+            TextFieldDefaults.DecorationBox(
+                value = query,
+                innerTextField = innerTextField,
+                enabled = true, // TextField 활성화/비활성화
+                singleLine = true, // TextField 한 줄로 표시
+                visualTransformation = VisualTransformation.None, // 텍스트 타입 (비밀번호, 전화번호 등)
+                placeholder = {
+                    if (query.isEmpty()) {
+                        Text(
+                            text = placeholderText,
+                            style = WepliTheme.typo.body2,
+                            color = WepliTheme.color.gray500,
+                        )
+                    }
+                }, // PlaceHolder (별도 Composable로 선언)
+                leadingIcon = {
+                    Icon(
+                        modifier = Modifier.size(20.dp),
+                        painter = painterResource(id = com.wepli.designsystem.R.drawable.ic_search),
+                        tint = WepliTheme.color.gray300,
+                        contentDescription = null
+                    )
+                }, // 텍스트 앞에 보여줄 아이콘 (별도 Composable로 선언)
+                trailingIcon = null, // 텍스트 끝에 보여줄 아이콘
+                colors = TextFieldDefaults.colors(
+                    unfocusedTextColor = WepliTheme.color.gray900,
+                    focusedTextColor = WepliTheme.color.gray900,
+                ), // TextField 색상
+                interactionSource = interactionSource, // ??
+                isError = false,
+                contentPadding = contentPadding,
+                container = {},
+            )
+        },
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+    )
+}
+
+@Preview
+@Composable
+fun SearchScreenPreview() {
+    SearchScreen()
+}
