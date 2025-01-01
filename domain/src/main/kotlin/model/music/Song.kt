3be5b466da6812a89591053e1713a2af1c8342d6
dongx._.2 <dongdong99@kakao.com>
2025-01-02 00:59:03 +0900

    refactor(song) : AppleSong -> Song으로 병합

--- domain/src/main/kotlin/model/music/Song.kt
+++ domain/src/main/kotlin/model/music/Song.kt
@@ -6,24 +6,17 @@ import common.DomainModel
  * 음악 정보
  *
  * @property title 제목
- * @property artist 아티스트
- * @property albumCoverImg 앨범 커버 이미지 URL
+ * @property artistName 아티스트
+ * @property coverImg 앨범 커버 이미지 URL
  */
 data class Song(
-    val title: String,
-    val artist: String,
-    val album: String,
-    val albumCoverImg: String,
-) : DomainModel
-
-data class AppleSong(
     val id: String,
-    val href: String,
     val title: String,
-    val albumName: String,
     val artistName: String,
+    val albumName: String,
+    val coverImg: String,
+    val href: String,
     val genres: List<String>,
     val durationMillis: Long,
-    val coverImg: String,
     // artwork
 ) : DomainModel
