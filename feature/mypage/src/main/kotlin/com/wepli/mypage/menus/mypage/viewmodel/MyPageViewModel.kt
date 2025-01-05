bd466dd19ec2659c5e608725ddedf486da1ad822
dongx._.2 <dongdong99@kakao.com>
2025-01-05 17:28:20 +0900

    refactor(mypage) : 유저 정보 조회 로직 연동

--- feature/mypage/src/main/kotlin/com/wepli/mypage/menus/mypage/viewmodel/MyPageViewModel.kt
+++ feature/mypage/src/main/kotlin/com/wepli/mypage/menus/mypage/viewmodel/MyPageViewModel.kt
@@ -1,27 +1,47 @@
 package com.wepli.mypage.menus.mypage.viewmodel
 
-import base.BaseViewModel
-import com.wepli.mypage.menus.mypage.state.MyPageState
-import com.wepli.shared.feature.mock.userMockData
+import base.BaseMviViewModel
+import base.Intent
+import base.SideEffect
+import base.UiState
+import com.wepli.shared.feature.uimodel.user.UserUiData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import model.user.User
+import repository.user.UserRepository
 import javax.inject.Inject
 
-@HiltViewModel
-class MyPageViewModel @Inject constructor() : BaseViewModel() {
+data class MyPageUiState(
+    val user: UserUiData
+) : UiState
 
-    private val _state: MutableStateFlow<MyPageState> = MutableStateFlow(MyPageState())
-    val state: StateFlow<MyPageState> = _state.asStateFlow()
+interface MyPageEffect : SideEffect
+
+interface MyPageIntent : Intent
+
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : BaseMviViewModel<MyPageUiState, MyPageEffect, MyPageIntent>(
+    initialState = MyPageUiState(user = UserUiData())
+){
+
+    override fun processIntent(intent: MyPageIntent) {
+        //
+    }
 
     init {
         loadUser()
     }
 
-    private fun loadUser() = launchWithHandler {
-        _state.value = MyPageState(
-            user = userMockData.random()
-        )
+    private fun loadUser() = intent {
+        launchWithHandler {
+            val user: User? = withContext(Dispatchers.IO) { userRepository.getUser() }
+            user?.let {
+                reduce { state.copy(user = UserUiData.fromDomain(it)) }
+            }
+        }
     }
 }
