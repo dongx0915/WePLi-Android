603c9fa966900fa2d13c5045d46de01de7730c39
dongx._.2 <dongdong99@kakao.com>
2025-01-17 00:17:44 +0900

    feat : 인기 차트(노래, 앨범, 플리 등) 조회 API 추가

--- data/src/main/kotlin/com/wepli/data/applemusic/AppleMusicApi.kt
+++ data/src/main/kotlin/com/wepli/data/applemusic/AppleMusicApi.kt
@@ -3,15 +3,24 @@ package com.wepli.data.applemusic
 import com.wepli.core.kotlin.FlowResult
 import retrofit2.http.GET
 import retrofit2.http.Query
-import wepli.domain.search.AppleSearchResponse
+import com.wepli.data.applemusic.response.AppleSearchResponse
 
 interface AppleMusicApi {
 
+    // 검색
     @GET("v1/catalog/kr/search")
     fun searchForCatalogResources(
         @Query("term") term: String, // 검색어
         @Query("limit") limit: Int = 25, // 반환 결과 수
         @Query("offset") offset: Int? = null, // 검색 결과의 시작 위치
         @Query("types") types: List<String> // 검색 결과의 타입 [activities, albums, apple-curators, artists, curators, music-videos, playlists, record-labels, songs, stations]
+    ): FlowResult<AppleSearchResponse>
+
+    // 인기 차트 조회
+    @GET("v1/catalog/kr/charts")
+    fun searchForCatalogCharts(
+        @Query("limit") limit: Int = 10, // 반환 결과 수
+        @Query("offset") offset: Int? = null, // 검색 결과의 시작 위치
+        @Query("types") types: List<String>, // 차트 타입 [albums, songs, playlists]
     ): FlowResult<AppleSearchResponse>
 }
