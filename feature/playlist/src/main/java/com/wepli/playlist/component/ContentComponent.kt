5fce2d88b9e3b827699be7dbd6f461e4b9071684
dongx._.2 <dongdong99@kakao.com>
2025-01-19 20:46:59 +0900

    refactor : 플레이리스트 수록곡 레이아웃 modifier 적용

--- feature/playlist/src/main/java/com/wepli/playlist/component/ContentComponent.kt
+++ feature/playlist/src/main/java/com/wepli/playlist/component/ContentComponent.kt
@@ -6,18 +6,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wepli.shared.feature.mock.songMockData
 import com.wepli.uimodel.artist.ArtistUiData
+import com.wepli.uimodel.music.SongUiData
 import custom.ArtistProfileListItem
 import custom.MusicItem
 import custom.MusicItemType
 import custom.TwoLineTitle
+import extensions.compose.toPx
 
 @Composable
 fun ArtistLayout(
@@ -45,18 +50,33 @@ fun ArtistLayout(
     }
 }
 
-@Preview
 @Composable
-fun PlaylistBsideTrackContent(modifier: Modifier = Modifier) {
+fun PlaylistBsideTrackContent(
+    modifier: Modifier = Modifier,
+    bSideTrack: List<SongUiData>
+) {
+    val imageSize = 52.dp
+
     Column(modifier = modifier) {
-        songMockData.forEachIndexed { index, song ->
+        bSideTrack.forEach { song ->
             MusicItem(
                 modifier = Modifier
                     .padding(vertical = 6.dp)
                     .padding(start = 20.dp, end = 16.dp),
-                musicItemType = MusicItemType.Normal(song),
+                imageModifier = Modifier
+                    .size(imageSize)
+                    .clip(RoundedCornerShape(3.dp)),
+                musicItemType = MusicItemType.Normal(song, imageSize.toPx()),
                 showMoreIcon = true
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun PlaylistBSideTrackContentPreview() {
+    PlaylistBsideTrackContent(
+        bSideTrack = songMockData
+    )
 }
