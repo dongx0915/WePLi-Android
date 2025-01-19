1c36845904bc994b4cf76a8febec051cea5f29bf
dongx._.2 <dongdong99@kakao.com>
2025-01-19 20:49:57 +0900

    feat : Playlist 요청 로직 추가

--- feature/playlist/src/main/java/com/wepli/playlist/PlaylistViewModel.kt
+++ feature/playlist/src/main/java/com/wepli/playlist/PlaylistViewModel.kt
@@ -1,51 +1,52 @@
 package com.wepli.playlist
 
-import base.BaseViewModel
-import com.wepli.playlist.state.PlaylistState
-import com.wepli.shared.feature.mock.artistMockData
-import com.wepli.shared.feature.mock.songMockData
+import android.util.Log
+import base.BaseMviViewModel
+import com.wepli.core.kotlin.suspendCollectResult
+import com.wepli.playlist.mvi.PlaylistEffect
+import com.wepli.playlist.mvi.PlaylistIntent
+import com.wepli.playlist.mvi.PlaylistUiState
 import com.wepli.shared.feature.uimodel.playlist.PlaylistUiData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import java.util.Date
+import repository.playlist.PlaylistRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class PlaylistViewModel @Inject constructor() : BaseViewModel() {
-
-    private val _state: MutableStateFlow<PlaylistState> = MutableStateFlow(PlaylistState())
-    val state: StateFlow<PlaylistState> = _state.asStateFlow()
+class PlaylistViewModel @Inject constructor(
+    private val playlistRepository: PlaylistRepository,
+) : BaseMviViewModel<PlaylistUiState, PlaylistEffect, PlaylistIntent>(
+    initialState = PlaylistUiState()
+) {
 
     init {
-        loadPlaylist()
+        getPlaylistById(2)
     }
 
-    fun toggleLiked() {
-        _state.update {
-            it.copy(
-                playlist = it.playlist.copy(
-                    isLiked = !it.playlist.isLiked
+    override fun processIntent(intent: PlaylistIntent) {
+        // TODO Implement processIntent
+    }
+
+    fun toggleLiked() = intent {
+        val likeState = state.playlist.isLiked
+        reduce {
+            state.copy(
+                playlist = state.playlist.copy(
+                    isLiked = !likeState
                 )
             )
         }
     }
 
-    private fun loadPlaylist() {
-        _state.value = PlaylistState(
-            playlist = PlaylistUiData(
-                id = 0,
-                title = "[Playlist] 졸릴 때 잠깨기 좋은 청량맛 노동요\uD83E\uDDCA\uD83C\uDF0A\uD83D\uDC99",
-                description = "나와 비슷한 취향을 가진 사람들이 올 한해 즐겨들었던 곡들을 모아봤어요. 다른 사람들은 어떤 노래를 듣는지 궁금할 때가 있잖아요. 특...나와 비슷한 취향을 가진 사람들이 올 한해 즐겨들었던 곡들을 모아봤어요. 다른 사람들은 어떤 노래를 듣는지 궁금할 때가 있잖아요. 특...나와 비슷한 취향을 가진 사람들이 올 한해 즐겨들었던 곡들을 모아봤어요. 다른 사람들은 어떤 노래를 듣는지 궁금할 때가 있잖아요. 특...나와 비슷한 취향을 가진 사람들이 올 한해 즐겨들었던 곡들을 모아봤어요. 다른 사람들은 어떤 노래를 듣는지 궁금할 때가 있잖아요. 특...",
-                coverImgUrl = "https://image.bugsm.co.kr/album/images/1000/41068/4106803.jpg",
-                author = "WePLi",
-                isLiked = true,
-                bSideTrack = songMockData,
-                artists = artistMockData,
-                createdAt = Date()
-            )
-        )
+    private fun getPlaylistById(id: Int) = intent {
+        launchWithHandler {
+            playlistRepository.getPlaylistById(id)
+                .suspendCollectResult(
+                    onSuccess = {
+                        reduce {
+                            state.copy(playlist = PlaylistUiData.fromDomain(it))
+                        }
+                    }
+                )
+        }
     }
 }
