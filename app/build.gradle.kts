7e79512ccbd6004df751f3fabd5323947a5900bc
dongx._.2 <dongdong99@kakao.com>
2025-01-19 19:06:19 +0900

    feat : Ktor Logging 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,6 @@ dependencies {
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.logging)
     implementation(libs.androidx.junit.ktx)
 }
