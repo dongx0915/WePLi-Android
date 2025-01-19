15bd1e106a030c52bee40ce7ab3aba95f60109ad
dongx._.2 <dongdong99@kakao.com>
2025-01-19 20:38:02 +0900

    feat : PlaylistMviState 추가

---
+++ feature/playlist/src/main/java/com/wepli/playlist/mvi/PlaylistState.kt
@@ -0,0 +1,14 @@
+package com.wepli.playlist.mvi
+
+import base.Intent
+import base.SideEffect
+import base.UiState
+import com.wepli.shared.feature.uimodel.playlist.PlaylistUiData
+
+data class PlaylistUiState(
+    val playlist: PlaylistUiData = PlaylistUiData(),
+) : UiState
+
+interface PlaylistEffect : SideEffect
+
+interface PlaylistIntent : Intent
--- feature/playlist/src/main/java/com/wepli/playlist/state/PlaylistState.kt
+++
@@ -1,7 +0,0 @@
-package com.wepli.playlist.state
-
-import com.wepli.shared.feature.uimodel.playlist.PlaylistUiData
-
-data class PlaylistState(
-    val playlist: PlaylistUiData = PlaylistUiData(),
-)
