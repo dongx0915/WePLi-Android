70d76ad6dd92d84fb2376ac5708842391d6c6e50
dongx._.2 <dongdong99@kakao.com>
2025-01-25 19:51:02 +0900

    feat : 공통 TextField 추가

--- designsystem/src/main/kotlin/textfield/TextFieldComponent.kt
+++ designsystem/src/main/kotlin/textfield/TextFieldComponent.kt
@@ -1,9 +1,15 @@
 package textfield
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -12,8 +18,10 @@ import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.OutlinedTextFieldDefaults.Container
+import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
@@ -22,99 +30,228 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wepli.designsystem.R
 import theme.WepliTheme
 
-// TODO 공통 TextField로 추출 필요
+sealed interface WepliTextFieldType {
+
+    @Composable
+    fun leadingIcon(): Painter?
+
+    @Composable
+    fun trailingIcon(): Painter?
+
+    fun minHeight(): Dp = 44.dp
+    fun maxHeight(): Dp = 44.dp
+
+    fun isEnabled(): Boolean = true
+
+    data object Normal : WepliTextFieldType {
+        @Composable
+        override fun leadingIcon(): Painter? = null
+
+        @Composable
+        override fun trailingIcon(): Painter? = null
+    }
+
+    data object Search : WepliTextFieldType {
+        @Composable
+        override fun leadingIcon(): Painter = painterResource(id = R.drawable.ic_search)
+
+        @Composable
+        override fun trailingIcon(): Painter? = null
+    }
+
+    data object SearchTwo : WepliTextFieldType {
+        @Composable
+        override fun leadingIcon(): Painter? = null
+
+        @Composable
+        override fun trailingIcon(): Painter = painterResource(id = R.drawable.ic_search)
+    }
+}
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchMusicTextField(
-    modifier: Modifier,
-    query: String,
+fun WepliTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    placeholder: String,
+    isError: Boolean = false,
+    singleLine: Boolean = false,
     readOnly: Boolean = false,
-    onQueryUpdate: (String) -> Unit,
-    onEnter: () -> Unit,
-    placeholderText: String = "",
+    onValueChanged: (String) -> Unit = { _ -> },
+    onEnter: () -> Unit = {},
+    onFocusChanged: (FocusState) -> Unit = {},
+    focusRequester: FocusRequester = remember { FocusRequester() },
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(onDone = { onEnter() }),
+    type: WepliTextFieldType,
 ) {
-    var textFieldValueState by remember { mutableStateOf(TextFieldValue(text = query)) }
+    var textFieldValueState by remember { mutableStateOf(TextFieldValue(text = value)) }
     val interactionSource = remember { MutableInteractionSource() }
     val customTextSelectionColors = TextSelectionColors(
         handleColor = WepliTheme.color.gray700,
         backgroundColor = WepliTheme.color.gray300,
     )
+    val textFieldColors = OutlinedTextFieldDefaults.colors(
+        // TextField 색상
+        focusedContainerColor = WepliTheme.color.gray000,
+        unfocusedContainerColor = WepliTheme.color.gray000,
 
-    LaunchedEffect(query) {
-        if (textFieldValueState.text != query) {
-            textFieldValueState = TextFieldValue(text = query)
+        focusedBorderColor = WepliTheme.color.gray000,
+        unfocusedBorderColor = WepliTheme.color.gray000,
+
+        unfocusedTextColor = WepliTheme.color.gray900,
+        focusedTextColor = WepliTheme.color.gray900,
+        selectionColors = TextSelectionColors(
+            handleColor = WepliTheme.color.gray900,
+            backgroundColor = WepliTheme.color.gray300,
+        ),
+    )
+
+    LaunchedEffect(value) {
+        if (textFieldValueState.text != value) {
+            textFieldValueState = textFieldValueState.copy(text = value)
         }
     }
 
-    CompositionLocalProvider(
-        LocalTextSelectionColors provides customTextSelectionColors
-    ) {
-        BasicTextField(
-            modifier = modifier
-                .clip(RoundedCornerShape(4.dp))
-                .background(WepliTheme.color.gray000),
-            readOnly = readOnly,
-            value = textFieldValueState,
-            onValueChange = { newQuery ->
-                textFieldValueState = newQuery
-                onQueryUpdate(newQuery.text)
-            },
-            textStyle = WepliTheme.typo.body2.copy(
-                color = WepliTheme.color.gray900,
-            ),
-            cursorBrush = SolidColor(WepliTheme.color.gray900),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(onDone = { onEnter() }),
-            decorationBox = @Composable { innerTextField ->
-                TextFieldDefaults.DecorationBox(
-                    value = query,
-                    innerTextField = innerTextField,
-                    enabled = true, // TextField 활성화/비활성화
-                    singleLine = true, // TextField 한 줄로 표시
-                    visualTransformation = VisualTransformation.None, // 텍스트 타입 (비밀번호, 전화번호 등)
-                    placeholder = { // PlaceHolder (별도 Composable로 선언)
-                        if (query.isEmpty()) {
+    Box(modifier = Modifier.wrapContentSize()) {
+        CompositionLocalProvider(
+            LocalTextSelectionColors provides customTextSelectionColors
+        ) {
+            BasicTextField(
+                value = textFieldValueState,
+                modifier = modifier
+                    .heightIn(
+                        min = type.minHeight(),
+                        max = type.maxHeight(),
+                    )
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .onFocusChanged { focusState -> onFocusChanged(focusState) },
+                onValueChange = { newValue ->
+                    textFieldValueState = newValue
+                    onValueChanged(newValue.text)
+                },
+                enabled = type.isEnabled(),
+                readOnly = readOnly,
+                textStyle = WepliTheme.typo.body2.copy(
+                    color = WepliTheme.color.gray900,
+                ),
+                cursorBrush = SolidColor(WepliTheme.color.gray900),
+                visualTransformation = VisualTransformation.None,
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                interactionSource = interactionSource,
+                singleLine = singleLine,
+                decorationBox = @Composable { innerTextField ->
+                    OutlinedTextFieldDefaults.DecorationBox(
+                        value = textFieldValueState.text,
+                        visualTransformation = VisualTransformation.None,
+                        innerTextField = innerTextField,
+                        placeholder = {
                             Text(
-                                text = placeholderText,
+                                text = placeholder,
                                 style = WepliTheme.typo.body2,
                                 color = WepliTheme.color.gray500,
                             )
-                        }
-                    },
-                    leadingIcon = { // 텍스트 앞에 보여줄 아이콘 (별도 Composable로 선언)
-                        Icon(
-                            modifier = Modifier.size(20.dp),
-                            painter = painterResource(id = R.drawable.ic_search),
-                            tint = WepliTheme.color.gray300,
-                            contentDescription = null
-                        )
-                    },
-                    trailingIcon = null, // 텍스트 끝에 보여줄 아이콘
-                    colors = TextFieldDefaults.colors(
-                        // TextField 색상
-                        unfocusedTextColor = WepliTheme.color.gray900,
-                        focusedTextColor = WepliTheme.color.gray900,
-                        selectionColors = TextSelectionColors(
-                            handleColor = WepliTheme.color.gray900,
-                            backgroundColor = WepliTheme.color.gray300,
-                        ),
-                    ),
-                    interactionSource = interactionSource, // ??
-                    isError = false,
-                    contentPadding = PaddingValues(all = 0.dp),
-                    container = {},
-                )
-            },
+                        },
+                        leadingIcon = type.leadingIcon()?.let {
+                            {
+                                Icon(
+                                    modifier = Modifier.size(20.dp),
+                                    painter = it,
+                                    tint = WepliTheme.color.gray300,
+                                    contentDescription = null
+                                )
+                            }
+                        },
+                        trailingIcon = type.trailingIcon()?.let {
+                            {
+                                Icon(
+                                    modifier = Modifier.size(20.dp),
+                                    painter = it,
+                                    tint = WepliTheme.color.gray300,
+                                    contentDescription = null
+                                )
+                            }
+                        },
+                        singleLine = singleLine,
+                        enabled = type.isEnabled(),
+                        isError = isError,
+                        interactionSource = interactionSource,
+                        colors = textFieldColors,
+                        contentPadding = if (singleLine) {
+                            PaddingValues(vertical = 0.dp, horizontal = 16.dp)
+                        } else {
+                            contentPadding()
+                        },
+                        container = {
+                            Container(
+                                enabled = type.isEnabled(),
+                                isError = isError,
+                                interactionSource = interactionSource,
+                                colors = textFieldColors,
+                                shape = RoundedCornerShape(4.dp),
+                                focusedBorderThickness = 0.dp,
+                            )
+                        },
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WepliTextFieldPreview() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        WepliTextField(
+            value = "",
+            onValueChanged = { _ -> },
+            onEnter = {},
+            onFocusChanged = {},
+            singleLine = true,
+            placeholder = "Placeholder",
+            type = WepliTextFieldType.Normal,
+        )
+
+        WepliTextField(
+            value = "",
+            onValueChanged = { _ -> },
+            onEnter = {},
+            onFocusChanged = {},
+            singleLine = true,
+            placeholder = "검색어를 입력하세요.",
+            type = WepliTextFieldType.Search,
+        )
+
+        WepliTextField(
+            value = "",
+            onValueChanged = { _ -> },
+            onEnter = {},
+            onFocusChanged = {},
+            singleLine = true,
+            placeholder = "검색어를 입력하세요.",
+            type = WepliTextFieldType.SearchTwo,
         )
     }
 }
