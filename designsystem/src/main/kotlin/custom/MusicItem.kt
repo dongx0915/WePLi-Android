d5670b52a85b6c572fd3c13b2994e7d0589e597e
dongx._.2 <dongdong99@kakao.com>
2025-01-26 16:24:17 +0900

    fix : 노래 아이템 Radius 누락 수정

--- designsystem/src/main/kotlin/custom/MusicItem.kt
+++ designsystem/src/main/kotlin/custom/MusicItem.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -83,7 +85,7 @@ fun MusicItem(
         modifier = modifier.height(52.dp),
     ) {
         AsyncImageWithPreview(
-            modifier = imageModifier,
+            modifier = imageModifier.clip(RoundedCornerShape(3.dp)),
             imageUrl = musicItemType.coverImgUrl,
             previewImage = painterResource(id = R.drawable.img_placeholder_album_cover),
             imageOverrideSize = 52.dp,
