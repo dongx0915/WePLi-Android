964b25247e4c2f4fb417852701210777d2791466
dongx._.2 <dongdong99@kakao.com>
2025-01-27 19:39:41 +0900

    feat : UserRepository id로 유저 정보 조회 로직 추가

--- data/src/main/kotlin/com/wepli/data/user/UserRepositoryImpl.kt
+++ data/src/main/kotlin/com/wepli/data/user/UserRepositoryImpl.kt
@@ -1,7 +1,11 @@
 package com.wepli.data.user
 
+import com.wepli.core.kotlin.FlowResult
 import com.wepli.data.datastore.DataStoreKey
 import com.wepli.data.datastore.local.DataStorePrefDataSource
+import com.wepli.data.network.toEntityResult
+import com.wepli.data.user.datasource.UserSupabaseDataSource
+import com.wepli.data.user.response.toUser
 import extensions.parseFromJson
 import extensions.toJsonString
 import model.user.User
@@ -10,8 +14,15 @@ import java.time.Instant
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
+    private val userSupabaseDataSource: UserSupabaseDataSource,
     private val dataStorePrefDataSource: DataStorePrefDataSource
 ) : UserRepository {
+
+    override suspend fun getUserById(id: String): FlowResult<User?> {
+        return userSupabaseDataSource.getUserById(id).toEntityResult {
+            it?.toUser()
+        }
+    }
 
     override suspend fun getUser(): User? {
         return dataStorePrefDataSource.getString(DataStoreKey.USER, "").parseFromJson<User>()
--- domain/src/main/kotlin/repository/user/UserRepository.kt
+++ domain/src/main/kotlin/repository/user/UserRepository.kt
@@ -1,9 +1,12 @@
 package repository.user
 
+import com.wepli.core.kotlin.FlowResult
 import model.user.User
 import java.time.Instant
 
 interface UserRepository {
+
+    suspend fun getUserById(id: String): FlowResult<User?>
 
     suspend fun getUser(): User?
     suspend fun setUserData(user: User)
