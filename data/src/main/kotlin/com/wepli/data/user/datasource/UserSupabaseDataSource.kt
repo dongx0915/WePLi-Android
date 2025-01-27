f71fc1070894b3b67597f74229b06700963b8289
dongx._.2 <dongdong99@kakao.com>
2025-01-27 19:38:56 +0900

    feat : UserSupabaseDataSource 추가

---
+++ data/src/main/kotlin/com/wepli/data/user/datasource/UserSupabaseDataSource.kt
@@ -0,0 +1,9 @@
+package com.wepli.data.user.datasource
+
+import com.wepli.core.kotlin.FlowResult
+import com.wepli.data.user.response.UserResponse
+
+interface UserSupabaseDataSource {
+
+    suspend fun getUserById(id: String): FlowResult<UserResponse?>
+}
