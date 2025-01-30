db45239f7273020940f32009db9f5bee0aaad7f9
dongx._.2 <dongdong99@kakao.com>
2025-01-30 22:35:38 +0900

    feat : SupabaseTable 상수 선언

--- data/src/main/kotlin/com/wepli/data/SupabaseTable.kt
+++ data/src/main/kotlin/com/wepli/data/SupabaseTable.kt
@@ -2,4 +2,9 @@ package com.wepli.data
 
 object SupabaseTable {
     const val USER_TABLE = "user"
+
+    const val SONG_TABLE = "song"
+
+    const val POST_TABLE = "posts"
+    const val POST_BSIDE_TRACK_TABLE = "post_bside_track"
 }
