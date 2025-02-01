38edf3190feada2c9334aef1dd8c6ed1ec5ef3be
dongx._.2 <dongdong99@kakao.com>
2025-02-01 19:59:55 +0900

    feat : 게시글 요청 ViewModel 로직 추가

--- feature/community/src/main/kotlin/com/wepli/community/main/CommunityViewModel.kt
+++ feature/community/src/main/kotlin/com/wepli/community/main/CommunityViewModel.kt
@@ -1,18 +1,25 @@
 package com.wepli.community.main
 
+import android.util.Log
 import base.BaseViewModel
 import com.wepli.community.main.state.CommunityMainState
-import com.wepli.shared.feature.mock.postMockData
+import com.wepli.core.kotlin.suspendCollectResult
 import com.wepli.shared.feature.mock.userMockData
+import com.wepli.shared.feature.uimodel.community.PostUiData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.update
+import repository.post.PostRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class CommunityViewModel @Inject constructor() : BaseViewModel() {
+class CommunityViewModel @Inject constructor(
+    private val postRepository: PostRepository,
+) : BaseViewModel() {
 
     private val _state = MutableStateFlow(CommunityMainState())
     val state: StateFlow<CommunityMainState> = _state.asStateFlow()
@@ -29,10 +36,18 @@ class CommunityViewModel @Inject constructor() : BaseViewModel() {
     }
 
     private fun loadPosts() = launchWithHandler {
-        val posts = List(100) { postMockData }.flatten().shuffled()
-
-        _state.update {
-            it.copy(posts = posts)
-        }
+        postRepository.getPosts()
+            .flowOn(Dispatchers.IO)
+            .suspendCollectResult(
+                onSuccess = { posts ->
+                    Log.d("CommunityViewModel", "loadPosts: $posts")
+                    _state.update {
+                        it.copy(posts = posts.map { PostUiData.fromDomain(it) })
+                    }
+                },
+                onFailure = {
+                    Log.e("CommunityViewModel", "loadPosts: $it")
+                }
+            )
     }
 }
