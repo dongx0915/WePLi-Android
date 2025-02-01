e03ba39c755591948c189c16accfcf7ce0073482
dongx._.2 <dongdong99@kakao.com>
2025-02-02 05:12:47 +0900

    chore : 테스트 코드 제거

--- data/src/main/kotlin/com/wepli/data/post/datasource/PostSupabaseDataSourceImpl.kt
+++ data/src/main/kotlin/com/wepli/data/post/datasource/PostSupabaseDataSourceImpl.kt
@@ -31,7 +31,6 @@ class PostSupabaseDataSourceImpl @Inject constructor(
 
     override fun getPosts(): FlowResult<List<PostResponse>> = flow {
         val result = runCatching {
-            throw Exception("Not implemented")
             supabase.postgrest[SupabaseTable.POST_VIEW].select().decodeList<PostResponse>()
         }
 
