366480ad3143c2d5c4cbafa219f2ed7e1e3d1b8f
dongx._.2 <dongdong99@kakao.com>
2025-02-09 17:52:09 +0900

    feat : App 모듈에 NameCard 모듈 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(projects.feature.community)
     implementation(projects.feature.playlist)
     implementation(projects.feature.mypage)
+    implementation(projects.feature.namecard)
     implementation(projects.data)
     implementation(projects.domain)
     implementation(projects.core.common)
