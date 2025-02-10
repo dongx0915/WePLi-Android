86d08d8d840285231fce34b469c59357298ee469
dongx._.2 <dongdong99@kakao.com>
2025-02-10 12:27:30 +0900

    feat : 페이지 설정 로직 추가 (state 내에서 수행)

--- feature/namecard/src/main/java/com/wepli/feature/namecard/detail/mvi/NameCardDetailMvi.kt
+++ feature/namecard/src/main/java/com/wepli/feature/namecard/detail/mvi/NameCardDetailMvi.kt
@@ -6,8 +6,21 @@ import base.UiState
 import com.wepli.uimodel.music.SongUiData
 
 data class NameCardDetailUiState(
+    private val _currentPage: Int = 0,
+    val totalPage: Int = 4,
     val selectedFavoriteSong: SongUiData? = null
-) : UiState
+) : UiState {
+    val currentPage: Int
+        get() = _currentPage
+
+    fun setNextPage(): NameCardDetailUiState {
+        return copy(_currentPage = (currentPage + 1).coerceIn(0, totalPage))
+    }
+
+    fun setPreviousPage(): NameCardDetailUiState {
+        return copy(_currentPage = (currentPage - 1).coerceIn(0, totalPage))
+    }
+}
 
 interface NameCardDetailEffect : SideEffect {
 
@@ -15,4 +28,8 @@ interface NameCardDetailEffect : SideEffect {
 
 interface NameCardDetailIntent : Intent {
     data class OnFavoriteSongSelected(val song: SongUiData) : NameCardDetailIntent
+    data object OnNextPage : NameCardDetailIntent
+    data object OnPreviousPage : NameCardDetailIntent
+    data object OnCompleteChapter : NameCardDetailIntent
+    data object NavigateToBack : NameCardDetailIntent
 }
