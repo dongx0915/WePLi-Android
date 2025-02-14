5cb2ce66273d309a768d09be49354e366c24531c
dongx._.2 <dongdong99@kakao.com>
2025-02-15 00:05:23 +0900

    chore : 미사용 import 제거

--- feature/namecard/src/main/java/com/wepli/feature/namecard/result/NameCardResultScreen.kt
+++ feature/namecard/src/main/java/com/wepli/feature/namecard/result/NameCardResultScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,7 +24,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import appbar.AppBarIcon
 import appbar.AppBarIconType
 import appbar.WepliAppBar
