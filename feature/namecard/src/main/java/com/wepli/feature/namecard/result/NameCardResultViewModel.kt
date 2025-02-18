7b1d4315f130c36091ee3fa5f4b2d3b863f751e6
dongx._.2 <dongdong99@kakao.com>
2025-02-19 00:27:27 +0900

    refactor : 명함 테스트 데이터 제거

--- feature/namecard/src/main/java/com/wepli/feature/namecard/result/NameCardResultViewModel.kt
+++ feature/namecard/src/main/java/com/wepli/feature/namecard/result/NameCardResultViewModel.kt
@@ -4,8 +4,6 @@ import base.BaseMviViewModel
 import com.wepli.feature.namecard.result.mvi.NameCardResultEffect
 import com.wepli.feature.namecard.result.mvi.NameCardResultIntent
 import com.wepli.feature.namecard.result.mvi.NameCardResultUiState
-import com.wepli.shared.feature.mock.songMockData
-import com.wepli.shared.feature.uimodel.namecard.NameCardUiData
 import com.wepli.shared.feature.uimodel.user.UserUiData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import repository.user.UserRepository
@@ -30,22 +28,9 @@ class NameCardResultViewModel @Inject constructor(
         }
     }
 
-
     private fun setUserInfo() = launch {
         userRepository.getUser()?.let {
             updateState { copy(user = UserUiData.fromDomain(it)) }
         }
-    }
-
-    private fun setNameCardInfo() {
-        val nameCardInfo = NameCardUiData(
-            nickname = "테스트 닉네임",
-            userTendency = "Melody Memories",
-            oneLineIntro = "테스트 문구입니다. 자신의 취향을 소개하는 문구를 작성해보세요.",
-            instagramId = "dongx._.2",
-            favoriteSong = songMockData.random()
-        )
-
-        updateState { copy(nameCardInfo = nameCardInfo) }
     }
 }
