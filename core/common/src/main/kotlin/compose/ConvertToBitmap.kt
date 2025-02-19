dc59f3f6fcdf5f3737292e540c8f093da5af9a3c
dongx._.2 <dongdong99@kakao.com>
2025-02-20 00:32:03 +0900

    feat : Composable -> Bitmap 변환하는 함수 추가

---
+++ core/common/src/main/kotlin/compose/ConvertToBitmap.kt
@@ -0,0 +1,29 @@
+package compose
+
+import android.graphics.Bitmap
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.drawToBitmap
+
+@Composable
+fun convertToBitmap(
+    targetContent: @Composable () -> Unit
+) : () -> Bitmap {
+    val context = LocalContext.current
+    val composeView = remember { ComposeView(context) }
+
+    fun captureBitmap(): Bitmap = composeView.drawToBitmap()
+
+    AndroidView(
+        factory = {
+            composeView.apply {
+                setContent { targetContent() }
+            }
+        },
+    )
+
+    return ::captureBitmap
+}
