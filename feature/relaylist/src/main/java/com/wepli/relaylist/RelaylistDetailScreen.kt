0e9631de604789d0b0e16c598af4f988ee8b163a
dongx._.2 <dongdong99@kakao.com>
2025-03-05 00:02:45 +0900

    feat : RelaylistDetailScreen 일부 추가

--- feature/relaylist/src/main/java/com/wepli/relaylist/RelaylistDetailScreen.kt
+++ feature/relaylist/src/main/java/com/wepli/relaylist/RelaylistDetailScreen.kt
@@ -1,17 +1,25 @@
 package com.wepli.relaylist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import appbar.ScrollableAppBar
 import appbar.WepliAppBar
-import com.wepli.shared.feature.mock.relaylistMockData
+import com.wepli.designsystem.R
+import com.wepli.shared.feature.mock.relaylistUiMockData
+import image.AsyncImageWithPreview
 import theme.WepliTheme
-
 
 @Composable
 fun RelaylistDetailScreenRoute() {
@@ -23,7 +31,7 @@ fun RelaylistDetailScreenRoute() {
 @Composable
 fun RelaylistDetailScreen() {
     val scrollState = rememberLazyListState()
-    val relaylist = relaylistMockData.first()
+    val relaylist = relaylistUiMockData.random()
 
     ScrollableAppBar(
         scrollState = scrollState,
@@ -39,8 +47,24 @@ fun RelaylistDetailScreen() {
             )
         }
     ) { paddingValue ->
-        Box {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(WepliTheme.color.black)) {
 
+            // 백그라운드
+            AsyncImageWithPreview(
+                imageUrl = relaylist.coverImgUrl,
+                contentScale = ContentScale.Fit,
+                previewImage = painterResource(id = R.drawable.img_placeholder_chuu_3),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .blur(20.dp),
+            )
+
+            // 릴레이리스트 콘텐츠
+            Column {
+
+            }
         }
     }
 }
