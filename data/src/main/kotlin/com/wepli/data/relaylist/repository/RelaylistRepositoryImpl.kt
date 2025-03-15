11b75160dcca6dc2c919e490a6d0da0495c80fd5
dongx._.2 <dongdong99@kakao.com>
2025-03-16 03:36:59 +0900

    feat : Relaylist 캐싱 로직 추가

--- data/src/main/kotlin/com/wepli/data/relaylist/repository/RelaylistRepositoryImpl.kt
+++ data/src/main/kotlin/com/wepli/data/relaylist/repository/RelaylistRepositoryImpl.kt
@@ -6,17 +6,27 @@ import com.wepli.data.network.toEntityResult
 import com.wepli.data.relaylist.datasource.remote.RelaylistDataSource
 import com.wepli.data.relaylist.response.toEntities
 import com.wepli.data.relaylist.response.toEntity
+import kotlinx.coroutines.flow.flowOf
 import model.relaylist.Relaylist
 import repository.relaylist.RelaylistRepository
+import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
 
 class RelaylistRepositoryImpl @Inject constructor(
     @SupabaseDataSource private val relaylistDataSource: RelaylistDataSource,
 ) : RelaylistRepository {
 
+    private var cachedRelaylist: AtomicReference<Relaylist> = AtomicReference(null)
+
     override fun getRelaylistById(id: Int): FlowResult<Relaylist> {
-        return relaylistDataSource.getRelaylistById(id).toEntityResult {
-            it.toEntity()
+        cachedRelaylist.getAndSet(null)?.let { relaylist ->
+            return flowOf(Result.success(relaylist))
+        }
+
+        return relaylistDataSource.getRelaylistById(id).toEntityResult { dto ->
+            dto.toEntity().also { newRelaylist ->
+                cachedRelaylist.set(newRelaylist)
+            }
         }
     }
 
