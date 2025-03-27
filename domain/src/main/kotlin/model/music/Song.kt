5bf4bffda40bac61fb10257996745e10f6a5bef8
dongx._.2 <dongdong99@kakao.com>
2025-03-28 01:31:18 +0900

    feat : 노래 속성 추가

--- domain/src/main/kotlin/model/music/Song.kt
+++ domain/src/main/kotlin/model/music/Song.kt
@@ -1,6 +1,7 @@
 package model.music
 
 import common.DomainModel
+import org.joda.time.LocalDate
 
 /**
  * 음악 정보
@@ -8,6 +9,10 @@ import common.DomainModel
  * @property title 제목
  * @property artistName 아티스트
  * @property coverImg 앨범 커버 이미지 URL
+ * @property composers 작곡가
+ * @property genres 장르
+ * @property url 노래 페이지 URL
+ * @property previewMusicUrl 미리듣기 URL
  */
 data class Song(
     val id: String,
@@ -15,8 +20,19 @@ data class Song(
     val artistName: String,
     val albumName: String,
     val coverImg: String,
-    val href: String,
+    val composers: List<String>,
     val genres: List<String>,
+    val url: String,
+    val previewMusicUrl: String,
+    val releaseDate: LocalDate,
     val durationMillis: Long,
+    val playParams: PlayParams,
+    val href: String,
     // artwork
-) : DomainModel
+) : DomainModel {
+
+    data class PlayParams(
+        val id: String,
+        val kind: String,
+    )
+}
