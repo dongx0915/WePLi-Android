4e365052595bfae71dc8abbedec3fbf28f4f497d
dongx._.2 <dongdong99@kakao.com>
2025-03-29 21:31:36 +0900

    feat : song, album 파싱 로직 추가

--- data/src/main/kotlin/com/wepli/data/applemusic/datasource/AppleMusicDataSourceImpl.kt
+++ data/src/main/kotlin/com/wepli/data/applemusic/datasource/AppleMusicDataSourceImpl.kt
@@ -1,11 +1,17 @@
 package com.wepli.data.applemusic.datasource
 
 import com.wepli.core.kotlin.flow.FlowResult
+import com.wepli.core.kotlin.flow.collectResult
 import com.wepli.data.applemusic.AppleMusicApi
 import com.wepli.data.applemusic.common.response.AppleAlbumResponse
 import com.wepli.data.applemusic.common.response.AppleSongResponse
+import com.wepli.data.applemusic.common.response.base.AppleDataWrapper
 import com.wepli.data.applemusic.response.AppleCatalogResponse
 import com.wepli.data.applemusic.response.AppleSearchResponse
+import com.wepli.data.network.toEntityResult
+import common.WePLiException
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AppleMusicDataSourceImpl @Inject constructor(
@@ -27,10 +33,14 @@ class AppleMusicDataSourceImpl @Inject constructor(
     }
 
     override fun getCatalogSong(songId: String): FlowResult<AppleSongResponse> {
-        return appleMusicApi.getCatalogSong(songId)
+        return appleMusicApi.getCatalogSong(songId).map { result ->
+            result.map { it.data.first() }
+        }
     }
 
     override fun getCatalogAlbum(albumId: String): FlowResult<AppleAlbumResponse> {
-        return appleMusicApi.getCatalogAlbum(albumId)
+        return appleMusicApi.getCatalogAlbum(albumId).map { result ->
+            result.map { it.data.first() }
+        }
     }
 }
