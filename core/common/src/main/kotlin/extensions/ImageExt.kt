5227d5f4565e6b26c4d57e952adf201b2fb8043e
dongx._.2 <dongdong99@kakao.com>
2025-04-13 21:45:37 +0900

    feat : 이미지 캐시 디렉토리 저장 로직 추가

--- core/common/src/main/kotlin/extensions/ImageExt.kt
+++ core/common/src/main/kotlin/extensions/ImageExt.kt
@@ -10,6 +10,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.toPixelMap
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
 
 fun ImageBitmap.toAndroidBitmap(): Bitmap {
     // 1. Compose의 PixelMap 가져오기
@@ -59,6 +62,31 @@ fun Bitmap.saveBitmapToFile(
 
         if (success) onSuccess(uri, absolutePath) else onFailure()
     } ?: {
+        onFailure()
+    }
+}
+
+fun Bitmap.saveBitmapToCache(
+    context: Context,
+    fileName: String,
+    onSuccess: (Uri, String) -> Unit,
+    onFailure: () -> Unit
+) {
+    val filename = "$fileName.png"
+    val file = File(context.cacheDir, filename)
+
+    try {
+        FileOutputStream(file).use { outputStream ->
+            val success = this.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            if (success) {
+                val uri = FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+                onSuccess(uri, file.absolutePath)
+            } else {
+                onFailure()
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
         onFailure()
     }
 }
