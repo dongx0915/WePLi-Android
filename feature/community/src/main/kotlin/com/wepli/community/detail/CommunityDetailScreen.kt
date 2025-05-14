3ad0a47535921923f44e20efb8e0f3209bf920f9
dongx._.2 <dongdong99@kakao.com>
2025-05-15 02:04:09 +0900

    fix : 커뮤니티 상세 뒤로가기 이벤트 누락건 추가

--- feature/community/src/main/kotlin/com/wepli/community/detail/CommunityDetailScreen.kt
+++ feature/community/src/main/kotlin/com/wepli/community/detail/CommunityDetailScreen.kt
@@ -3,7 +3,6 @@ package com.wepli.community.detail
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -73,11 +72,11 @@ fun CommunityDetailScreenRoute(
     CommunityDetailScreen(
         state = state,
         sendAction = viewModel::processIntent,
-        navOnBack = { }
+        navOnBack = navOnBack
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommunityDetailScreen(
     state: CommunityDetailState,
