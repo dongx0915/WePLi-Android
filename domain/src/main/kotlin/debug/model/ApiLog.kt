45a19377143f5ebfe10b5062822055aaa16eab6f
dongx._.2 <dongdong99@kakao.com>
2025-05-27 00:31:10 +0900

    feat : ApiLog 도메인 모델 추가

--- domain/src/main/kotlin/debug/model/ApiLog.kt
+++ domain/src/main/kotlin/debug/model/ApiLog.kt
@@ -1,7 +1,9 @@
 package debug.model
 
+import java.util.Locale
+
 data class ApiLog(
-    val method: String,
+    val method: ApiMethod,
     val url: String,
     val requestHeaders: String,
     val requestBody: String,
@@ -9,3 +11,21 @@ data class ApiLog(
     val responseBody: String,
     val durationMs: Long
 )
+
+enum class ApiMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    UNKNOWN;
+
+    companion object {
+        fun fromString(method: String): ApiMethod {
+            return try {
+                valueOf(method.uppercase(Locale.getDefault()))
+            } catch (e: IllegalArgumentException) {
+                UNKNOWN
+            }
+        }
+    }
+}
