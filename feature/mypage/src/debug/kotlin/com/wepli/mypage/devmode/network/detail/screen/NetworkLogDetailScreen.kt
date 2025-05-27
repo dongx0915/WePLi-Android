73072e0bb73a2087cb6cf35a55484da167586db0
dongx._.2 <dongdong99@kakao.com>
2025-05-28 03:47:15 +0900

    refactor : 터치 영역 조정

--- feature/mypage/src/debug/kotlin/com/wepli/mypage/devmode/network/detail/screen/NetworkLogDetailScreen.kt
+++ feature/mypage/src/debug/kotlin/com/wepli/mypage/devmode/network/detail/screen/NetworkLogDetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -222,6 +223,7 @@ private fun CollapsingComponent(
     contents: String,
     modifier: Modifier = Modifier
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
     var expanded by remember { mutableStateOf(false) }
     val rotationAngle by animateFloatAsState(
         targetValue = if (expanded) 180f else 0f,
@@ -231,7 +233,6 @@ private fun CollapsingComponent(
     Column(
         modifier = modifier
             .animateContentSize()
-            .clickable { expanded = !expanded } // 클릭 시 toggle
             .fillMaxWidth()
             .clip(RoundedCornerShape(8.dp))
             .background(color = WepliTheme.color.gray050)
@@ -242,6 +243,10 @@ private fun CollapsingComponent(
         // Header
         Row(
             verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable(
+                interactionSource = interactionSource,
+                indication = null,
+            ) { expanded = !expanded }
         ) {
             Text(
                 text = title,
