00126762066de402f1eef539e9a266023ac66f42
dongx._.2 <dongdong99@kakao.com>
2025-05-28 01:51:03 +0900

    refacotr : import 정리

--- feature/mypage/src/debug/kotlin/com/wepli/mypage/devmode/network/detail/navigation/NetworkLogNavigation.kt
+++ feature/mypage/src/debug/kotlin/com/wepli/mypage/devmode/network/detail/navigation/NetworkLogNavigation.kt
@@ -1,6 +1,5 @@
 package com.wepli.mypage.devmode.network.detail.navigation
 
-import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -8,7 +7,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import animation.transition.ScreenTransitions
 import com.wepli.mypage.devmode.network.detail.screen.NetworkLogDetailScreenRoute
-import com.wepli.mypage.devmode.network.main.screen.NetworkLogDebugScreenRoute
 import com.wepli.navigator.feature.mypage.NetworkLogRoute
 
 fun NavController.navigateToNetworkLogDetail(apiLogId: String) {
