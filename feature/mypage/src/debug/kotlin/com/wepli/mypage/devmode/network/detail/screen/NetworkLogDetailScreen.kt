ca9e83caf6870ad6ae07822d3a88c3d6c9e3c76d
dongx._.2 <dongdong99@kakao.com>
2025-05-28 02:13:21 +0900

    feat : Detail 데이터 연동

--- feature/mypage/src/debug/kotlin/com/wepli/mypage/devmode/network/detail/screen/NetworkLogDetailScreen.kt
+++ feature/mypage/src/debug/kotlin/com/wepli/mypage/devmode/network/detail/screen/NetworkLogDetailScreen.kt
@@ -1,21 +1,31 @@
 package com.wepli.mypage.devmode.network.detail.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import appbar.WepliAppBar
 import com.wepli.mypage.devmode.mock.mockApiLogs
 import com.wepli.mypage.devmode.network.detail.viewmodel.NetworkLogDetailIntent
 import com.wepli.mypage.devmode.network.detail.viewmodel.NetworkLogDetailState
 import com.wepli.mypage.devmode.network.detail.viewmodel.NetworkLogDetailViewModel
+import com.wepli.mypage.devmode.network.main.component.StatusTag
+import com.wepli.mypage.devmode.network.main.enums.toColor
+import debug.model.ApiLog
 import org.orbitmvi.orbit.compose.collectAsState
 import theme.WepliTheme
 
@@ -52,6 +62,8 @@ fun NetworkLogDetailScreen(
     state: NetworkLogDetailState,
     navOnBack: () -> Unit
 ) {
+    state.apiLog ?: return
+
     Scaffold(
         containerColor = WepliTheme.color.black,
         topBar = {
@@ -63,8 +75,74 @@ fun NetworkLogDetailScreen(
             )
         }
     ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(vertical = 20.dp, horizontal = 20.dp)
+        ) {
+            ApiInfoHeader(apiLog = state.apiLog)
         }
+    }
+}
+
+@Composable
+private fun ApiInfoHeader(apiLog: ApiLog) {
+    Column {
+        // Status Tag
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            Text(
+                text = apiLog.method.name.uppercase(),
+                style = WepliTheme.typo.subTitle2,
+                color = apiLog.method.toColor(),
+            )
+
+            StatusTag(apiLog.responseCode)
+        }
+
+        // Url
+        Text(
+            text = apiLog.decodedUrl,
+            style = WepliTheme.typo.subTitle5,
+            color = WepliTheme.color.gray800,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+
+        ApiSubInfoComponent(
+            title = "Time:",
+            data = apiLog.formattedStartTime(),
+            modifier = Modifier.padding(top = 24.dp)
+        )
+
+        ApiSubInfoComponent(
+            title = "Duration:",
+            data = apiLog.durationMs.toString() + "ms",
+            modifier = Modifier.padding(top = 12.dp)
+        )
+    }
+}
+
+@Composable
+private fun ApiSubInfoComponent(title: String, data: String, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier,
+    ) {
+        Text(
+            text = title,
+            style = WepliTheme.typo.body3,
+            color = WepliTheme.color.gray600,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = data,
+            style = WepliTheme.typo.body3,
+            color = WepliTheme.color.gray800,
+        )
     }
 }
