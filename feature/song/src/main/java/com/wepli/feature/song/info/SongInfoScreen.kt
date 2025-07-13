e2fb30b0e08f3bf2abd3a6ab275ede8d002f552a
김동현 <dongdong99@kakao.com>
2025-07-13 20:49:23 +0900

    refactor : 미사용 import 제거

--- feature/song/src/main/java/com/wepli/feature/song/info/SongInfoScreen.kt
+++ feature/song/src/main/java/com/wepli/feature/song/info/SongInfoScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
