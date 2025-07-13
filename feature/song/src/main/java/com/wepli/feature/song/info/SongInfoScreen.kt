f49e134f83f12eca9b2387f5738b39ccd6bc1035
김동현 <dongdong99@kakao.com>
2025-07-13 15:26:31 +0900

    feat : 접힌 상태에서는 유튜브 중지되도록 수정

--- feature/song/src/main/java/com/wepli/feature/song/info/SongInfoScreen.kt
+++ feature/song/src/main/java/com/wepli/feature/song/info/SongInfoScreen.kt
@@ -24,6 +24,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -136,18 +139,32 @@ fun SongInfoScreen(
 @Composable
 fun YoutubeVideoPlayer(
     videoId: String = "",
+    isExpanded: Boolean = false,
     lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
     modifier: Modifier = Modifier
 ) {
+    var youTubePlayer: YouTubePlayer? by remember { mutableStateOf(null) }
+    
+    LaunchedEffect(isExpanded) {
+        youTubePlayer?.let { player ->
+            if (isExpanded) {
+                player.play()
+            } else {
+                player.pause()
+            }
+        }
+    }
+    
     AndroidView(
         modifier = modifier.fillMaxWidth(),
         factory = {
-            YouTubePlayerView(context = it).apply { 
+            YouTubePlayerView(context = it).apply {
                 lifecycleOwner.lifecycle.addObserver(this)
 
                 addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-                    override fun onReady(youTubePlayer: YouTubePlayer) {
-                        youTubePlayer.loadVideo(videoId, 0f)
+                    override fun onReady(player: YouTubePlayer) {
+                        youTubePlayer = player
+                        player.cueVideo(videoId, 0f)
                     }
                 })
             }
@@ -178,6 +195,7 @@ fun MusicVideoInfoLayout(
         if (isExpanded) {
             YoutubeVideoPlayer(
                 videoId = "riWBCpP14ek",
+                isExpanded = isExpanded,
                 modifier = Modifier.clip(RoundedCornerShape(16.dp))
             )
         }
