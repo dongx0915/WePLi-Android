b2447547205ac3cfd386bc5a232d06de62a245bc
김동현 <dongdong99@kakao.com>
2025-09-20 05:48:03 +0900

    feat : MockData 변경

--- shared/feature/src/main/java/com/wepli/shared/feature/mock/RelaylistMockData.kt
+++ shared/feature/src/main/java/com/wepli/shared/feature/mock/RelaylistMockData.kt
@@ -2,6 +2,7 @@ package com.wepli.shared.feature.mock
 
 import android.annotation.SuppressLint
 import com.wepli.shared.feature.uimodel.relaylist.RelaylistUiData
+import com.wepli.uimodel.music.toDomain
 import model.relaylist.Relaylist
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
@@ -15,7 +16,7 @@ val relaylistMockData = listOf(
         title = "그 시절, 아무 걱정 없었던 그때",
         description = "어린 시절이 생각나는 노래 모음",
         coverImgUrl = "https://img.29cm.co.kr/cms/202411/11efa2f103b3aea1afbcaff1c242f37c.jpg?width=2000&q=75",
-        bSideTrack = emptyList(),
+        bSideTrack = songMockData.map { it.toDomain() },
         songCount = 10,
         voteCount = 100,
         endDate = DateTime.parse("2025.03.12", formatter),
@@ -27,7 +28,7 @@ val relaylistMockData = listOf(
         title = "감성 한 잔, 카페 BGM",
         description = "트는 순간 바로 인생샷",
         coverImgUrl = "https://img.29cm.co.kr/cms/202411/11efa330c272ddb9afbc0dbc604d203e.jpg?q=75",
-        bSideTrack = emptyList(),
+        bSideTrack = songMockData.map { it.toDomain() },
         songCount = 10,
         voteCount = 100,
         endDate = DateTime.parse("2025.03.12", formatter),
@@ -39,7 +40,7 @@ val relaylistMockData = listOf(
         title = "첫 눈 오던 날",
         description = "추운 날씨에 어울리는 한 겨울 리메이크 곡",
         coverImgUrl = "https://img.29cm.co.kr/cms/202411/11efa32fd888d0eb86c37fb1ce8c821c.JPG?q=75",
-        bSideTrack = emptyList(),
+        bSideTrack = songMockData.map { it.toDomain() },
         songCount = 10,
         voteCount = 100,
         endDate = DateTime.parse("2025.03.12", formatter),
@@ -51,7 +52,7 @@ val relaylistMockData = listOf(
         title = "썸녀한테 추천해줄 감성 싱잉랩",
         description = "카톡하다 대화 소재 떨어 졌을때",
         coverImgUrl = "https://img.29cm.co.kr/cms/202411/11ef9ff687cc9f379876cf8486967618.jpg?q=75",
-        bSideTrack = emptyList(),
+        bSideTrack = songMockData.map { it.toDomain() },
         songCount = 10,
         voteCount = 100,
         endDate = DateTime.parse("2025.03.12", formatter),
