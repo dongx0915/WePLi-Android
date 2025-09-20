028688273a695e0284e10a5109f78ca6d56587da
김동현 <dongdong99@kakao.com>
2025-09-21 03:10:27 +0900

    refactor : 바텀 네비게이션 바 배경색 투명도 조절

--- app/src/main/java/com/wepli/app/MainActivity.kt
+++ app/src/main/java/com/wepli/app/MainActivity.kt
@@ -153,7 +153,7 @@ fun BottomNavigationBar(
     currentRoute: String?,
     hazeState: HazeState // hazeState 매개변수 추가
 ) {
-    val bottomNavColor = WepliTheme.color.black.copy(0.7f)
+    val bottomNavColor = WepliTheme.color.black.copy(0.92f)
     val density = LocalDensity.current
     val navBarHeight = with(density) { WindowInsets.navigationBars.getBottom(this).toDp() }
 
