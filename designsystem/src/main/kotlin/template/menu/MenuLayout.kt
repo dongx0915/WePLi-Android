1ab48eac15e7012dc09755bb24378b80a449ed04
김동현 <dongdong99@kakao.com>
2025-09-23 00:31:18 +0900

    feat : Switch 메뉴 추가 중

--- designsystem/src/main/kotlin/template/menu/MenuLayout.kt
+++ designsystem/src/main/kotlin/template/menu/MenuLayout.kt
@@ -1,22 +1,49 @@
 package template.menu
 
+import androidx.compose.animation.core.exponentialDecay
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.anchoredDraggable
+import androidx.compose.foundation.gestures.animateTo
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import base.Intent
-import com.wepli.core.resources.R as CoreR
+import component.switch.WepliSwitch
+import kotlinx.coroutines.launch
 import theme.WepliTheme
+import kotlin.math.roundToInt
+import com.wepli.core.resources.R as CoreR
 
 @Composable
 fun MenuLayout(
@@ -79,6 +106,34 @@ fun MenuComponent(
             painter = painterResource(id = CoreR.drawable.ic_arrow_forward),
             tint = WepliTheme.color.gray600,
             contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MenuSwitchComponent(
+    modifier: Modifier = Modifier,
+    title: String = "메뉴 콘텐츠",
+    onClickMenu: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .clickable { onClickMenu() }
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = title,
+            style = WepliTheme.typo.body3,
+            color = WepliTheme.color.gray600
+        )
+
+        WepliSwitch(
+            checked = true,
+            onCheckedChange = {},
         )
     }
 }
--- feature/devmode/src/main/kotlin/com/wepli/feature/devmode/main/screen/DevModeScreen.kt
+++ feature/devmode/src/main/kotlin/com/wepli/feature/devmode/main/screen/DevModeScreen.kt
@@ -1,18 +1,32 @@
 package com.wepli.feature.devmode.main.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import appbar.WepliAppBar
 import com.wepli.feature.devmode.R
+import com.wepli.feature.devmode.main.navigation.DevModeRoute
+import template.menu.MenuComponent
+import template.menu.MenuLayout
+import template.menu.MenuTitleComponent
 import theme.WepliTheme
 
 
 @Composable
 fun DevModeScreenRoute() {
 
+}
+
+@Preview
+@Composable
+private fun DevModeScreenPreview() {
+    DevModeScreen { }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,7 +45,13 @@ private fun DevModeScreen(
             )
         }
     ) { paddingValues ->
-
+        Column(
+            modifier = Modifier.padding(paddingValues))
+        {
+            MenuTitleComponent(title = "로그")
+            MenuComponent(title = "네트워크 로그", onClickMenu = {  })
+            MenuComponent(title = "ScreenNameViewer 활성화", onClickMenu = {  })
+        }
         paddingValues
     }
 }
