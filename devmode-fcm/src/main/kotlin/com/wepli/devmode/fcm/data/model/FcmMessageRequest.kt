46f1a61d0bba3e808ff3648a76bb4de7c284786d
dongx._.2 <dongdong99@kakao.com>
2025-09-30 15:03:58 +0900

    feat : Request 클래스 Serializable 선언

--- devmode-fcm/src/main/kotlin/com/wepli/devmode/fcm/data/model/FcmMessageRequest.kt
+++ devmode-fcm/src/main/kotlin/com/wepli/devmode/fcm/data/model/FcmMessageRequest.kt
@@ -1,19 +1,26 @@
 package com.wepli.devmode.fcm.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FcmMessageRequest(
     val message: Message
 ) {
+    @Serializable
     data class Message(
         val token: String,
-        val notification: Notification,
-        val android: AndroidConfig? = null
+        val notification: Notification? = null,
+        val android: AndroidConfig? = null,
+        val data: String? = null,
     )
 
+    @Serializable
     data class Notification(
         val title: String,
         val body: String,
     )
 
+    @Serializable
     data class AndroidConfig(
         val priority: String = "high",
     )
