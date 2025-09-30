7bc07f160e789c2a1c193021c0406520a01059e4
김동현 <dongdong99@kakao.com>
2025-09-30 23:58:38 +0900

    feat : AppBar 추가

--- devmode-fcm/src/main/kotlin/com/wepli/devmode/fcm/presentation/DevFcmPushScreen.kt
+++ devmode-fcm/src/main/kotlin/com/wepli/devmode/fcm/presentation/DevFcmPushScreen.kt
@@ -2,18 +2,57 @@ package com.wepli.devmode.fcm.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.wepli.devmode.fcm.presentation.component.AppBarIcon
+import com.wepli.devmode.fcm.R
 
 
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DevFcmPushScreen() {
     Scaffold(
         containerColor = Color.Black,
         topBar = {
-
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "FCM 푸시 발송",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 16.sp,
+                            lineHeight = 20.sp,
+                        ),
+                        color = Color.White,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                navigationIcon = {
+                    AppBarIcon(
+                        iconResource = R.drawable.ic_arrow_back,
+                        onClick = {}
+                    )
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Black,
+                    navigationIconContentColor = Color.White,
+                    actionIconContentColor = Color.White,
+                    titleContentColor = Color.White
+                )
+            )
         }
     ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
