f6aaad13acfebf515d41e595a8f0606471c59e01
김동현 <dongdong99@kakao.com>
2025-09-30 23:49:14 +0900

    feat : FCM 푸시 발송 스크린 추가

---
+++ devmode-fcm/src/main/kotlin/com/wepli/devmode/fcm/presentation/DevFcmPushScreen.kt
@@ -0,0 +1,23 @@
+package com.wepli.devmode.fcm.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun DevFcmPushScreen() {
+    Scaffold(
+        containerColor = Color.Black,
+        topBar = {
+
+        }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+
+        }
+    }
+}
